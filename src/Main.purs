a1b98b8f15834e8b085e45c67f8d969503462f12
David Oliver <t3tris.d3vil@gmail.com>
2020-03-06 14:40:25 -0600

    Stuff

--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,11 @@
 module Main where
 
 import Prelude
-
 import Data.Array as A
 import Data.Foldable (class Foldable)
 import Data.List (List)
 import Effect (Effect)
+import Effect.Console (log)
 import Node.ChildProcess (defaultSpawnOptions, inherit, spawn)
 import Options.Applicative.Builder (forwardOptions, header, info, metavar, progDesc, strArgument)
 import Options.Applicative.Extra (execParser, helper)
@@ -15,27 +15,31 @@ import Options.Applicative.Types (Parser, many)
 α :: forall f. Foldable f => (forall a. f a -> Array a)
 α = A.fromFoldable
 
-type Opts = {
-  -- The destination branch name
-  dest :: String,
-  -- Options to be forwarded to git push
-  forwards :: List String
-}
+type Opts
+  = { -- The destination branch name
+      dest :: String
+    , -- Options to be forwarded to git push
+      forwards :: List String
+    }
 
 parseOpts :: Parser Opts
 parseOpts = ado
   dest <- strArgument (metavar "<dest name>")
   forwards <- many (strArgument (metavar "<options forwarded to git push>"))
-in { dest, forwards }
+  in { dest, forwards }
 
 main :: Effect Unit
-main = do
-  { dest, forwards } <- execParser $ info (parseOpts <**> helper) (
-    header "git push-as"
-    <> progDesc "Push a branch to git with a different name."
-    <> forwardOptions
-  )
-  pure unit <*
+main =
+  do
+    { dest, forwards } <-
+      execParser
+        $ info (parseOpts <**> helper)
+            ( header "git push-as"
+                <> progDesc "Push a branch to git with a different name."
+                <> forwardOptions
+            )
+    log (">> git push origin HEAD:refs/heads/" <> dest)
     spawn "git"
       ([ "push", "origin", "HEAD:refs/heads/" <> dest ] <> α forwards)
       (defaultSpawnOptions { stdio = inherit })
+    *> pure unit
